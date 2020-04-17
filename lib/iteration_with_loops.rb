e6461ca593e2ead4c1d250bae31bf57ff212d4e3
Daniel Cooper <cooperdaniel5141@gmail.com>
2020-04-17 07:35:55 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -3,6 +3,7 @@ def join_nested_strings(src)
   # Combine all Strings present in the AoA into a single value and return it
   solution = ""
   src.each do |item|
+    puts item
     if item.is_a? String
       solution += item
     end
